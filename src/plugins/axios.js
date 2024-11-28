0aaaf4575a7f5ef5d9a0ad6d4e250891aa253fa0
Ananda Noviesta Wijaya <anandawijayanoviesta@gmail.com>
2024-11-28 16:29:16 +0700

    Update Deployment Frontend

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/store/authStore'
 
 let apiClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://inventory-management-backend-mu.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     }
